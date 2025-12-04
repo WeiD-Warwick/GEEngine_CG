22024898268a17aea3950c78e1a14c3fa3f9d079
WeiD-Warwick <u5726890@live.warwick.ac.uk>
2025-12-04 15:24:58 +0000

    optimize performance

--- GEEngine/Src/Shaders/VS.hlsl
+++ GEEngine/Src/Shaders/VS.hlsl
@@ -4,27 +4,24 @@ cbuffer staticMeshBuffer : register(b0)
     float4x4 VP;
 };
 
-struct VS_INPUT
-{
-    float4 Pos : POSITION;
+struct VS_INPUT {
+    float3 Pos : POSITION;
     float3 Normal : NORMAL;
     float3 Tangent : TANGENT;
     float2 TexCoords : TEXCOORD;
 };
 
-struct PS_INPUT
-{
+struct PS_INPUT {
     float4 Pos : SV_POSITION;
     float3 Normal : NORMAL;
     float3 Tangent : TANGENT;
     float2 TexCoords : TEXCOORD;
 };
 
-PS_INPUT VS(VS_INPUT input)
-{
+PS_INPUT VS(VS_INPUT input) {
     PS_INPUT output;
 
-    output.Pos = mul(input.Pos, W);
+    output.Pos = mul(float4(input.Pos, 1.0f), W);
     output.Pos = mul(output.Pos, VP);
     output.Normal = mul(input.Normal, W);
     output.Tangent = mul(input.Tangent, W);
